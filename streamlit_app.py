5f10f0bcf10973e6f4724627a070380f5c228315
Dejeaux <135067765+Dejeaux@users.noreply.github.com>
2023-05-30 15:51:43 -0500

    Update streamlit_app.py
    Add icons & toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-boiled free-range egg')
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket smoothie')
+streamlit.text(' 🐔 Hard-boiled free-range egg')
+streamlit.text('🥑🍞 Avacodo Toast')

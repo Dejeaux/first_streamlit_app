ea57d9cc95fd7e4c1ba1ae1399778d92cc980f40
Dejeaux <135067765+Dejeaux@users.noreply.github.com>
2023-06-02 09:02:11 -0500

    Update streamlit_app.py
    normalize output

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,4 +31,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# standardize output
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output normalize data
+streamlit.dataframe(fruityvice_normalized)
+
 

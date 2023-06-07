e372964a595e3c2062d67df6f311843e5694e204
Dejeaux <135067765+Dejeaux@users.noreply.github.com>
2023-06-07 15:41:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import snowflake.connector
 from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Display the table on the page.
 
@@ -23,24 +24,17 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #streamlit.dataframe(fruits_to_show)
-#new header for api response
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-streamlit.write('The user entered',fruit_choice)
 
-#import requests
-#output to screen
-streamlit.dataframe(fruityvice_normalized)   
-# new header for api response
-streamlit.header( "Fruityvice Fruit Advice!" )
-try :
-    fruit_choice = streamlit.text_input( 'What fruit would you like information about?' )
+#new header for api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
-        streamlit.error( "Please select a fruit to get information" )
+        streamlit.error("Please select a fruit to get information")
     else :
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvoice_normalized)
+        streamlit.dataframe(fruityvice_normalized)
 
 except URLerror as e:
     streamlit.error()

1ead1932835e7fdeae9dc2d4d645cdb89d810bb7
Dejeaux <135067765+Dejeaux@users.noreply.github.com>
2023-06-07 14:38:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output to screen
 streamlit.dataframe(fruityvice_normalized)   
+ streamlit.stop()      
 
 except URLerror as e:
     streamlit.error()
- streamlit.stop()      
 
 #create fruityvice function
 def get_fruity_vice_data(this_fruit_choice):
